eccf4e597a8e0239f78c8f67c02f0f11bbe42006
Dayana <43152649+dayanalily@users.noreply.github.com>
2019-07-08 16:56:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const app  = express()
+const productRouter = require('./routes/product');
+
+//settings
+app.set('json spaces', 4)
+
+//middleeares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+//para formularios
+app.unsubscribe(bodyParser.urlencoded({extended: false }))
+
+
+// routers
+app.use('/products',productRouter);
+
+
+
+//estar serve
+app.listen(3000, () => {
+    console.log("server port ", 3000);
+    
+})
+
